1c26ce1491551f2a1232edd35900297d72f38bca
Agrah <agrahgupta1@gmail.com>
2024-09-16 12:03:29 +0530

    infoSoft

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Montserrat', 'sans-serif'],
+        display: ['Poppins', 'sans-serif'],
+      },
+     
+  },
+  },
+  plugins: [],
+}
+
